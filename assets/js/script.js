53ad1a83ab1d3bf9f7d9e99a71ad60c2addb8622
samavm <88936498+samavm@users.noreply.github.com>
2023-06-08 10:43:09 +0400

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,21 @@ document
       '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
     ]);
   });
+
+
+
+
+  $(function () {
+    $(document).scroll(function () {
+        var $nav = $(".head");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+  // mobile scroll color change
+  $(function () {
+    $(document).scroll(function () {
+        var $mobile = $(".mobile");
+        $mobile.toggleClass('scrolled', $(this).scrollTop() > $mobile.height());
+      });
+  });
